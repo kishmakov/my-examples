2a5815c63cb07ea8b34b1b66c8f3ac7151ccf980
Kirill Shmakov <kirill.shmakov@jetbrains.com>
2019-03-15 11:44:26 +0300

    Exception possibility added.

--- my-jvm-coroutines/src/main/kotlin/demo/Main.kt
+++ my-jvm-coroutines/src/main/kotlin/demo/Main.kt
@@ -2,7 +2,19 @@ package demo
 
 import kotlinx.coroutines.*
 
+fun g(n: Int): Int {
+    if (n < 0)
+        throw Exception("What do you want?")
+    return f(n - 1) + g(n - 3)
+}
+
+fun f(n: Int): Int {
+    return g(n - 1) + f(n - 2)
+}
+
 fun main() {
+    // f(10) // exception producing call
+
     GlobalScope.launch {
         delay(1000L)
         println("<coroutine 1>") // breakpoint
