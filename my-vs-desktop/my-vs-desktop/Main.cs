26453ab80087bf2f53f1d86537f3561293c10a81
Kirill Shmakov <kirill.shmakov@jetbrains.com>
2019-03-15 17:16:45 +0300

    An VS project added.

---
+++ my-vs-desktop/my-vs-desktop/Main.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using AppKit;
+
+namespace myvsdesktop
+{
+    static class MainClass
+    {
+        static int g(int n)
+        {
+            if (n < 0)
+            {
+                throw new Exception("What do you want?");
+            }
+
+            if (n == 0)
+            {
+                return 10;
+            } 
+            else
+            {
+                return g(n - 1) + f(n - 2);
+            }
+
+        }
+
+        static int f(int n)
+        {
+            return g(n - 1) + f(n - 2);
+        }
+
+        static void Main(string[] args)
+        {
+            f(10); // throwing function
+            NSApplication.Init();
+            NSApplication.Main(args);
+        }
+    }
+}
