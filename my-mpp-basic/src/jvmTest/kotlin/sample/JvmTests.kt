cf77f25c58e3f6de60b3adba6c9d64f7cc6395bc
Kirill Shmakov <kirill.shmakov@jetbrains.com>
2020-01-29 14:46:00 +0300

    Rename tests for JVM source set.

--- my-mpp-basic/src/jvmTest/kotlin/sample/JvmTests.kt
+++ my-mpp-basic/src/jvmTest/kotlin/sample/JvmTests.kt
@@ -3,7 +3,7 @@ package sample
 import kotlin.test.Test
 import kotlin.test.assertTrue
 
-class SampleTestsJvm {
+class JvmTests {
     @Test
     fun testHello() {
         assertTrue("Jacob" in hello("Jacob"))
