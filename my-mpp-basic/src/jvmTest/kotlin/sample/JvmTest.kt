902bf1291e4e1936911b830606c14892e3b93d3a
Kirill Shmakov <kirill.shmakov@jetbrains.com>
2020-11-27 11:05:34 +0300

    Enrich tests for debugging testing

--- my-mpp-basic/src/jvmTest/kotlin/sample/JvmTest.kt
+++ my-mpp-basic/src/jvmTest/kotlin/sample/JvmTest.kt
@@ -1,11 +1,27 @@
 package sample
 
 import kotlin.test.Test
+import kotlin.test.assertEquals
 import kotlin.test.assertTrue
+
+fun ask1() = buildString {
+    val x = "2"
+    append(x + 39)
+}
+
+fun ask2(): String {
+    val x = "2"
+    return x + 39
+}
 
 class JvmTest {
     @Test
     fun testHello() {
         assertTrue("Jacob" in hello("Jacob"))
+
+        val a1 = ask1()
+        val a2 = ask2()
+
+        assertEquals(a1, a2)
     }
 }
--- my-mpp-basic/src/macos1Test/kotlin/sample/Macos1Test.kt
+++ my-mpp-basic/src/macos1Test/kotlin/sample/Macos1Test.kt
@@ -1,7 +1,18 @@
 package sample
 
 import kotlin.test.Test
+import kotlin.test.assertEquals
 import kotlin.test.assertTrue
+
+fun ask1(): String = buildString {
+    val x = "2"
+    append(x + 39)
+}
+
+fun ask2(): String {
+    val x = "2"
+    return x + 39
+}
 
 class Macos1Test {
     @Test
@@ -9,13 +20,20 @@ class Macos1Test {
         val location = currentLocation()
         val what = question("What's up here at $location")
 
+        val pair = Pair(location, what)
+
         assertTrue("up" in what)
+
+        val a1 = ask1()
+        val a2 = ask2()
+
+        assertEquals(a1, a2)
     }
 
     @Test
     fun testHow() {
         val how = question("How are you?", arrayOf("Jim", "Bob"))
 
-        assertTrue("are" in how)
+        assertTrue("are2" in how)
     }
 }
