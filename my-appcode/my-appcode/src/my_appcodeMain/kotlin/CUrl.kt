f753d5b744f62a7bd07b1bbe4e64e1f71837c6b3
Kirill Shmakov <kirill.shmakov@jetbrains.com>
2021-10-01 23:47:48 +0300

    Remove old AC project.

--- my-appcode/my-appcode/src/my_appcodeMain/kotlin/CUrl.kt
+++
@@ -1,7 +0,0 @@
-package sample.libcurl
-
-fun pickAnUrl(args: Array<String>): String {
-    val i = args.size + 10
-    val ii = i * (i + 1) + 2
-    return if (ii > 112) args[0] else "https://google.com"
-}
